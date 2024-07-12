5a1beac633b7d6b69a46984a678af2b58914fde2
Jin Park <jin@jinpark.net>
2024-07-12 14:53:52 +0200

    wip

--- src/viewModels/ResetViewNavigationControl.js
+++ src/viewModels/ResetViewNavigationControl.js
@@ -174,6 +174,113 @@ ResetViewNavigationControl.prototype.resetView = function () {
   this.isActive = false;
 };
 
+ResetViewNavigationControl.prototype.resetCameraView = function () {
+  // this.terria.analytics.logEvent('navigation', 'click', 'reset');
+  if (this.navigationLocked) {
+    return;
+  }
+  var scene = this.terria.scene;
+
+  var sscc = scene.screenSpaceCameraController;
+  if (!sscc.enableInputs) {
+    return;
+  }
+
+  this.isActive = true;
+
+  var camera = scene.camera;
+
+  if (defined(this.terria.trackedEntity)) {
+    // when tracking do not reset to default view but to default view of tracked entity
+    var trackedEntity = this.terria.trackedEntity;
+    this.terria.trackedEntity = undefined;
+    this.terria.trackedEntity = trackedEntity;
+  } else {
+    // reset to a default position or view defined in the options
+    const duration = this.terria.options.duration
+      ? this.terria.options.duration
+      : 3;
+    if (defaultResetView) {
+      const orientation = this.terria.options.orientation
+        ? this.terria.options.orientation
+        : {
+            heading: CesiumMath.toRadians(5.729578),
+          };
+      if (
+        defaultResetView &&
+        defaultResetView instanceof Cartographic
+      ) {
+        camera.flyTo({
+          destination: scene.globe.ellipsoid.cartographicToCartesian(
+            defaultResetView
+          ),
+          orientation,
+          duration,
+        });
+      } else if (
+        defaultResetView &&
+        defaultResetView instanceof Rectangle
+      ) {
+        try {
+          Rectangle.validate(defaultResetView);
+          camera.flyTo({
+            destination: defaultResetView,
+            orientation,
+            duration,
+            complete: this.resetSuccess,
+          });
+        } catch (e) {
+          console.log(
+            "Cesium-navigation/ResetViewNavigationControl:   options.defaultResetView Cesium rectangle is  invalid!"
+          );
+        }
+      } else if (
+        defaultResetView &&
+        defaultResetView.isCesium3DTileset 
+      ) {
+        try {
+          this.terria.zoomTo(
+            defaultResetView,
+          );
+        } catch (e) {
+          console.log(
+            "Cesium-navigation/ResetViewNavigationControl:   options.defaultResetView Cesium tileset is invalid!"
+          );
+        }
+      } else if (
+        defaultResetView &&
+        defaultResetView instanceof Entity 
+      ) {
+        try {
+          this.terria.zoomTo(
+            defaultResetView,
+          );
+        } catch (e) {
+          console.log(
+            "Cesium-navigation/ResetViewNavigationControl:   options.defaultResetView Entity is invalid!"
+          );
+        }
+      } else if (
+        defaultResetView &&
+        defaultResetView.destination &&
+        defaultResetView.orientation 
+      ) {
+        camera.flyTo({
+          destination: defaultResetView.destination,
+          orientation: defaultResetView.orientation,
+          duration,
+          complete: this.resetSuccess,
+        });
+      }
+    } else if (typeof camera.flyHome === "function") {
+      camera.flyHome(1);
+    } else {
+      camera.flyTo({ destination: Camera.DEFAULT_VIEW_RECTANGLE, duration });
+    }
+  }
+  this.isActive = false;
+};
+
 /**
  * When implemented in a derived class, performs an action when the user clicks
  * on this control
